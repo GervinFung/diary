d60510c0a03dc2bc6b3784d291a8f527810a50df
GervinFung <gervinfungdaxuen@gmail.com>
2022-03-30 16:04:55 +0800

    chore(): added prettier for blade & php, concurrently package

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,6 +12,8 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+    .postCss('resources/css/app.css', 'public/css')
+    .postCss('resources/css/journal.css', 'public/css')
+    .postCss('resources/css/footer.css', 'public/css')
+    .postCss('resources/css/header.css', 'public/css')
+    .postCss('resources/css/home.css', 'public/css');
