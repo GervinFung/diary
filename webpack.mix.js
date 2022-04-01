b4fd1ea681c1e28e1c344eb219eb15ac9645f2ab
Pang Jia Jin <86699859+deviljin1009@users.noreply.github.com>
2022-04-01 17:37:25 +0800

    Modify webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -6,19 +6,15 @@ const mix = require('laravel-mix');
  |--------------------------------------------------------------------------
  |
  | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
+ | for your Laravel applications. By default, we are compiling the CSS
  | file for the application as well as bundling up all the JS files.
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css')
-    .postCss('resources/css/journal.css', 'public/css')
-    .postCss('resources/css/footer.css', 'public/css')
-    .postCss('resources/css/header.css', 'public/css')
-<<<<<<< HEAD
-    .postCss('resources/css/home.css', 'public/css')
-    .postCss('resources/css/journals.css', 'public/css');
-=======
-    .postCss('resources/css/home.css', 'public/css');
->>>>>>> 2751ea3b5a34c9dc97b0d5b96a16f7e1d6f18afb
+ mix.js('resources/js/app.js', 'public/js')
+ .postCss('resources/css/app.css', 'public/css')
+ .postCss('resources/css/journal.css', 'public/css')
+ .postCss('resources/css/footer.css', 'public/css')
+ .postCss('resources/css/header.css', 'public/css')
+ .postCss('resources/css/home.css', 'public/css')
+ .postCss('resources/css/addJournal.css', 'public/css');
